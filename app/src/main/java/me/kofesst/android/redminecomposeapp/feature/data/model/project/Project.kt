550595cba1bdd69ecb937f6c0009f968bf3e09da
kof3st <xkof3st.mail@gmail.com>
2022-05-04 22:40:31 +0300

    Изменён дизайн карточки проекта

--- app/src/main/java/me/kofesst/android/redminecomposeapp/feature/data/model/project/Project.kt
+++ app/src/main/java/me/kofesst/android/redminecomposeapp/feature/data/model/project/Project.kt
@@ -1,14 +1,15 @@
 package me.kofesst.android.redminecomposeapp.feature.data.model.project
 
 import me.kofesst.android.redminecomposeapp.feature.data.model.CustomField
+import me.kofesst.android.redminecomposeapp.feature.data.repository.util.DateTime
 import java.util.*
 
 data class Project(
-    val created_on: Date,
+    val created_on: DateTime,
     val custom_fields: List<CustomField>,
     val description: String,
     val id: Int,
     val identifier: String,
     val name: String,
-    val updated_on: String
+    val updated_on: DateTime
 )
--- app/src/main/java/me/kofesst/android/redminecomposeapp/feature/presentation/project/list/ProjectsScreen.kt
+++ app/src/main/java/me/kofesst/android/redminecomposeapp/feature/presentation/project/list/ProjectsScreen.kt
@@ -1,11 +1,9 @@
 package me.kofesst.android.redminecomposeapp.feature.presentation.project.list
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -13,11 +11,14 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import me.kofesst.android.redminecomposeapp.feature.data.model.project.Project
 import me.kofesst.android.redminecomposeapp.feature.domain.util.LoadingResult
+import me.kofesst.android.redminecomposeapp.feature.domain.util.formatDate
+import me.kofesst.android.redminecomposeapp.feature.presentation.ClickableCard
 import me.kofesst.android.redminecomposeapp.feature.presentation.DefaultSwipeRefresh
 import me.kofesst.android.redminecomposeapp.feature.presentation.Screen
 
@@ -67,17 +68,21 @@ fun ProjectItem(
     onItemClick: () -> Unit
 ) {
     Box(modifier = Modifier.padding(10.dp)) {
-        Card(
-            elevation = 8.dp,
-            modifier = Modifier
-                .fillMaxWidth()
-                .clickable { onItemClick() }
+        ClickableCard(
+            onClick = onItemClick,
+            modifier = Modifier.fillMaxWidth()
         ) {
-            Text(
-                text = project.name,
-                style = MaterialTheme.typography.body1,
-                modifier = Modifier.padding(16.dp)
-            )
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(
+                    text = project.name,
+                    style = MaterialTheme.typography.h6,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = "Обновлён ${project.updated_on.formatDate(showTime = true)}",
+                    style = MaterialTheme.typography.body1
+                )
+            }
         }
     }
 }
