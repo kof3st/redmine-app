9ceb33c954370d8dbd358a8ab495137eba8d950a
kof3st <xkof3st.mail@gmail.com>
2022-05-04 14:57:35 +0300

    Изменены цвета приложения

--- app/src/main/java/me/kofesst/android/redminecomposeapp/ui/theme/Color.kt
+++ app/src/main/java/me/kofesst/android/redminecomposeapp/ui/theme/Color.kt
@@ -6,3 +6,6 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val SiteSoft = Color(0xFFE94035)
+val SiteSoftVariant = Color(0xFFFB4E26)
--- app/src/main/java/me/kofesst/android/redminecomposeapp/ui/theme/Theme.kt
+++ app/src/main/java/me/kofesst/android/redminecomposeapp/ui/theme/Theme.kt
@@ -5,17 +5,22 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = SiteSoftVariant,
+    primaryVariant = SiteSoft,
+    secondary = SiteSoftVariant,
+    onPrimary = Color.White,
+    onSecondary = Color.White
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = SiteSoftVariant,
+    primaryVariant = SiteSoft,
+    secondary = SiteSoftVariant,
+    onPrimary = Color.White,
+    onSecondary = Color.White
 
     /* Other default colors to override
     background = Color.White,
