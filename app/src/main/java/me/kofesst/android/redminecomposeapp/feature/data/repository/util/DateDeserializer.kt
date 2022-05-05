87541ed71d503f39b9b76d66ebe79f08dc1bfff0
kof3st <xkof3st.mail@gmail.com>
2022-05-05 19:27:46 +0300

    Фикс парсинга даты без времени

--- app/src/main/java/me/kofesst/android/redminecomposeapp/feature/data/repository/util/DateDeserializer.kt
+++ app/src/main/java/me/kofesst/android/redminecomposeapp/feature/data/repository/util/DateDeserializer.kt
@@ -3,7 +3,7 @@ package me.kofesst.android.redminecomposeapp.feature.data.repository.util
 import java.util.*
 
 class DateDeserializer : CustomDateDeserializer<Date>(
-    dateFormat = "yyyy/MM/dd HH:mm:ss Z"
+    dateFormat = "yyyy/MM/dd"
 ) {
     override fun getFromDate(date: Date): Date {
         return date
